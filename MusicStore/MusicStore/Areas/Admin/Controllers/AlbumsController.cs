c4b05695b6c8a4d7247549842b2d7fcddc7f8bad
raf-bergs <r0752792@student.thomasmore.be>
2022-03-10 17:32:39 +0100

    music store 1 done

---
+++ MusicStore/MusicStore/Areas/Admin/Controllers/AlbumsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class AlbumsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
